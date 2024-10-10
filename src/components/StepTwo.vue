7fe42b995cdee3b0cbd15e0dc88961e8a38def95
ErasonUtkuCikmaz <utku.cikmaz@erason.de>
2024-10-10 17:36:51 +0200

    adding error messages for better UX

--- src/components/StepTwo.vue
+++ src/components/StepTwo.vue
@@ -3,31 +3,37 @@
     <h1 class="text-4xl font-bold mb-8 text-gray-800">WAS KOCHEN WIR HEUTE?</h1>
 
     <div class="bg-white rounded-lg shadow-lg p-6 w-1/2">
-      <h2 class="text-2xl font-semibold mb-4 text-gray-700">{{ randomMeal.name }}</h2>
-      <div class="flex justify-start">
-        <div class="w-1/2 m-2">
-          <img
-            :src="randomMeal.image"
-            alt="picture of the food"
-            class="w-full h-48 object-cover rounded-md"
-          />
-        </div>
-        <div class="w-1/2 grid justify-center">
-          <p class="text-gray-600 mb-2">
-            <strong class="font-semibold">Aufwand: {{ randomMeal.effort }}</strong>
-          </p>
-          <p class="text-gray-600 mb-2">
-            <strong class="font-semibold">Kosten: {{ randomMeal.cost }}</strong>
-          </p>
-          <p class="text-gray-600 mb-4">
-            <strong class="font-semibold">Vegetarisch: {{ randomMeal.veggie }}</strong>
-          </p>
+      <div v-if="randomMeal">
+        <h2 class="text-2xl font-semibold mb-4 text-gray-700">{{ randomMeal.name }}</h2>
+        <div class="flex justify-start">
+          <div class="w-1/2 m-2">
+            <img
+              :src="randomMeal.image"
+              alt="picture of the food"
+              class="w-full h-48 object-cover rounded-md"
+              @error="setNoImage"
+            />
+          </div>
+          <div class="w-1/2 grid justify-center">
+            <p class="text-gray-600 mb-2">
+              <strong class="font-semibold">Aufwand: {{ randomMeal.effort }}</strong>
+            </p>
+            <p class="text-gray-600 mb-2">
+              <strong class="font-semibold">Kosten: {{ randomMeal.cost }}</strong>
+            </p>
+            <p class="text-gray-600 mb-4">
+              <strong class="font-semibold">Vegetarisch: {{ randomMeal.veggie }}</strong>
+            </p>
 
-          <h3 class="text-xl font-medium mb-2 text-gray-700">Einkaufsliste</h3>
-          <ul class="list-disc list-inside text-gray-600">
-            <li v-for="item in randomMeal.ingredients" :key="item">{{ item }}</li>
-          </ul>
+            <h3 class="text-xl font-medium mb-2 text-gray-700">Einkaufsliste</h3>
+            <ul class="list-decimal list-inside text-gray-600">
+              <li v-for="item in randomMeal.ingredients" :key="item">{{ item }}</li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div v-else class="flex items-center justify-center">
+        <p class="my-8">Es gibt keine Mahlzeit unter dieser Auswahl</p>
       </div>
     </div>
   </div>
@@ -50,6 +56,11 @@
 
 <script setup>
 import { defineEmits, defineProps } from 'vue'
+import noImage from '@/assets/no-image.jpg'
+
+const setNoImage = (event) => {
+  event.target.src = noImage
+}
 
 defineProps({
   randomMeal: Object
